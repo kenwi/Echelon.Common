1515aebba25cbd3b9e945af9b94eb417fd11f58a
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2022-03-29 04:50:20 +0200

    String extension

---
+++ Extensions/StringEx.cs
@@ -0,0 +1,8 @@
+﻿namespace Echelon.Common.Extensions
+{
+    internal static class StringEx
+    {
+        public static string StripBackslashes(this string value)
+            => value.Replace(@"\\", @"\");
+    }
+}
