8b6098776238d7a22548cff229d431190abbe0a0
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2022-08-20 06:00:51 +0200

    Refactor and remove unused variable

--- Modules/SpotifyModule.cs
+++ Modules/SpotifyModule.cs
@@ -10,30 +10,22 @@ namespace Echelon.Bot.Services
     [Group("spotify")]
     public class SpotifyModule : ModuleBase<SocketCommandContext>
     {
-        private readonly IServiceProvider serviceProvider;
         private readonly IMessageWriter messageWriter;
         private readonly JsonService jsonService;
         private readonly SpotifyCallbackService callbackService;
         private readonly SpotifyService spotifyService;
+        private readonly DiscordService discordService;
 
         public SpotifyModule(IServiceProvider serviceProvider, IMessageWriter messageWriter)
         {
-            this.serviceProvider = serviceProvider;
             this.messageWriter = messageWriter;
 
             jsonService = serviceProvider.GetRequiredService<JsonService>();
             callbackService = serviceProvider.GetRequiredService<SpotifyCallbackService>();
             spotifyService = serviceProvider.GetRequiredService<SpotifyService>();
+            discordService = serviceProvider.GetRequiredService<DiscordService>();
 
             messageWriter.Write("SpotifyModule Started");
-        }
-
-        private async void PrintHelp(ulong channelId)
-        {
-            await ReplyAsync("Please provide a channel-id and playlist-id or link to a playlist"
-                + "```!spotify register <discord-channel-id> <spotify-playlist-id-or-link>```"
-                + "For this specific channel example: "
-                + $"```!spotify register {channelId} https://open.spotify.com/playlist/4a16Ug0LyXkAAlVZah5X2k?si=0e79cae526094196```");
         }
 
         [Command("register")]
@@ -58,14 +50,10 @@ namespace Echelon.Bot.Services
                     ServerName = Context.Guild.Name,
                     Challenge = challenge
                 });
+                
                 await jsonService.SaveItems(items);
-
-                var code = uri.Split("code=").Last();
                 await callbackService.StartAuthorizationProcess(verifier, challenge);
-
                 await ReplyAsync($"Registered channel **{Context.Channel.Name}**. Please verify your Spotify account with the link received by PM");
-
-                var discordService = serviceProvider.GetRequiredService<DiscordService>();
                 discordService.SendMessage($"Please login to Spotify with this link: {uri}", Context.User.Id, true);
             }
             catch (Exception ex)
