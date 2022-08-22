a4d6c6557b7a212d7ae0d6e10086be123f2ce74c
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2022-08-22 03:26:11 +0200

    Add playlistname as argument

--- Modules/SpotifyModule.cs
+++ Modules/SpotifyModule.cs
@@ -28,7 +28,7 @@ namespace Echelon.Bot.Services
         }
 
         [Command("register")]
-        public async Task Register()
+        public async Task Register(string playlistName = "")
         {
             if (!Context.IsUserInModeratorRole())
                 return;
@@ -39,21 +39,33 @@ namespace Echelon.Bot.Services
                 var uri = callbackService.CreateLoginRequestUri(challenge);
                 var channels = await jsonService.GetChannels();
 
-                channels.Add(Context.Channel.Id.ToString(), new SpotifyItem
+                if (!channels.TryGetValue(Context.Channel.Id, out var channel))
                 {
-                    ChannelId = Context.Channel.Id.ToString(),
+                    await Context.Channel.SendMessageAsync("This channel is already registered");
+                    return;
+                }
+
+                if (channels.TryAdd(Context.Channel.Id, new SpotifyItem
+                {
+                    ChannelId = Context.Channel.Id,
                     ChannelName = Context.Channel.Name,
-                    OwnerId = Context.User.Id.ToString(),
+                    OwnerId = Context.User.Id,
                     OwnerName = Context.User.Username,
                     ServerName = Context.Guild.Name,
                     PlaylistName = $"{Context.Guild.Name} - {Context.Channel.Name}",
                     Challenge = challenge
-                });
+                }))
+                {
+                    await jsonService.SaveChannels(channels);
 
-                await jsonService.SaveChannels(channels);
-                await callbackService.StartAuthorizationProcess(verifier, challenge);
-                await ReplyAsync($"Registered channel **{Context.Channel.Name}**. Please verify your Spotify account with the link received by PM");
-                discordService.SendMessage($"Please login to Spotify with this link: {uri}", Context.User.Id, true);
+                    if (string.IsNullOrEmpty(playlistName))
+                        await callbackService.StartAuthorizationProcess(verifier, challenge);
+                    else
+                        await callbackService.StartAuthorizationProcess(verifier, challenge, playlistName);
+
+                    await ReplyAsync($"Registered channel **{Context.Channel.Name}**. Please verify your Spotify account with the link received by PM");
+                    discordService.SendMessage($"Please login to Spotify with this link: {uri}", Context.User.Id, true);
+                }
             }
             catch (Exception ex)
             {
@@ -70,15 +82,13 @@ namespace Echelon.Bot.Services
             try
             {
                 var channels = await jsonService.GetChannels();
-                var channel = channels.FirstOrDefault(x => x.Key == Context.Channel.Id.ToString()).Value;
-
-                if (channel == null)
+                if (!channels.TryGetValue(Context.Channel.Id, out var channel))
                 {
                     await ReplyAsync($"Channel **{Context.Channel.Name}** is not registered");
                     return;
                 }
 
-                channels.Remove(Context.Channel.Id.ToString());
+                channels.Remove(Context.Channel.Id);
                 await jsonService.SaveChannels(channels);
                 await ReplyAsync($"Unregistered channel **{Context.Channel.Name}**");
             }
@@ -88,8 +98,8 @@ namespace Echelon.Bot.Services
             }
         }
 
-        [Command("create")]
-        public async Task Create(string name)
+        [Command("createplaylist")]
+        public async Task CreatePlaylist(string name)
         {
             if (!Context.IsUserInModeratorRole())
                 return;
@@ -98,8 +108,7 @@ namespace Echelon.Bot.Services
             try
             {
                 var channels = await jsonService.GetChannels();
-                channels.TryGetValue(Context.Channel.Id.ToString(), out var channel);
-                if (channel is null)
+                if (!channels.TryGetValue(Context.Channel.Id, out var channel))
                 {
                     await ReplyAsync($"Channel **{Context.Channel.Name}** is not registered");
                     return;
@@ -111,11 +120,59 @@ namespace Echelon.Bot.Services
             {
                 discordService.SendMessage(ex.Message, channelId);
                 messageWriter.Write(ex.Message);
-            }            
+            }
+        }
+
+        [Command("playlist")]
+        public async Task GetChannelPlaylist()
+        {
+            if (!Context.IsUserInModeratorRole())
+                return;
+
+            ulong channelId = 0;
+            try
+            {
+                var channels = await jsonService.GetChannels();
+                if (!channels.TryGetValue(Context.Channel.Id, out var channel))
+                {
+                    await ReplyAsync($"Channel **{Context.Channel.Name}** is not registered");
+                    return;
+                }
+                await ReplyAsync($"https://open.spotify.com/playlist/{channel.PlaylistId}");
+            }
+            catch (Exception ex)
+            {
+                discordService.SendMessage(ex.Message, channelId);
+                messageWriter.Write(ex.Message);
+            }
+        }
+
+        [Command("scope")]
+        public async Task GetScope()
+        {
+            if (!Context.IsUserInModeratorRole())
+                return;
+
+            ulong channelId = 0;
+            try
+            {
+                var channels = await jsonService.GetChannels();
+                if (!channels.TryGetValue(Context.Channel.Id, out var channel))
+                {
+                    await ReplyAsync($"Channel **{Context.Channel.Name}** is not registered");
+                    return;
+                }
+                await ReplyAsync($"Channel Token Scope: {channel.Token!.Scope}");
+            }
+            catch (Exception ex)
+            {
+                discordService.SendMessage(ex.Message, channelId);
+                messageWriter.Write(ex.Message);
+            }
         }
 
         [Command("rename")]
-        public async Task Rename(string name)
+        public async Task Rename(string newName)
         {
             if (!Context.IsUserInModeratorRole())
                 return;
@@ -123,20 +180,19 @@ namespace Echelon.Bot.Services
             try
             {
                 var channels = await jsonService.GetChannels();
-                channels.TryGetValue(Context.Channel.Id.ToString(), out var channel);
-                if (channel is null)
+                if (!channels.TryGetValue(Context.Channel.Id, out var channel))
                 {
                     await ReplyAsync($"Channel **{Context.Channel.Name}** is not registered");
                     return;
                 }
 
                 var oldname = channel.PlaylistName;
-                channel.PlaylistName = name;
+                channel.PlaylistName = newName;
                 channels[channel.ChannelId] = channel;
                 await jsonService.SaveChannels(channels);
 
-                await spotifyService.RenamePlaylist(Context.Channel.Id, channel.PlaylistId, name);
-                await ReplyAsync($"Renamed playlist **{oldname}** to **{name}**");
+                await spotifyService.RenamePlaylist(Context.Channel.Id, channel.PlaylistId, newName);
+                await ReplyAsync($"Renamed playlist **{oldname}** to **{newName}**");
             }
             catch (Exception ex)
             {
@@ -153,13 +209,12 @@ namespace Echelon.Bot.Services
             try
             {
                 var channels = await jsonService.GetChannels();
-                channels.TryGetValue(Context.Channel.Id.ToString(), out var channel);
-                if (channel is null)
+                if (!channels.TryGetValue(Context.Channel.Id, out var channel))
                 {
                     messageWriter.Write("Failed getting item");
                     return;
                 }
-                
+
                 await spotifyService.PlayNextSong(Context.Channel.Id);
                 await ReplyAsync("Playing next track");
             }
